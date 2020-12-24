6dd9e7c763a2ea104170ae9bf37d9d82e594a3af
Gia Huy <nguyengiahuy16@gmail.com>
2020-12-24 19:58:43 +0700

    upd

--- release/release_server/helpers/store.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.remove = exports.tokenOpening = void 0;
-exports.tokenOpening = [];
-const remove = (socket) => {
-    exports.tokenOpening = exports.tokenOpening.filter(o => o.client != socket);
-};
-exports.remove = remove;
-//# sourceMappingURL=store.js.map
--- release/release_server/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./boots");
-//# sourceMappingURL=index.js.map
